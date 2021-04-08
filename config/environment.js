e39ba9b759cbfa1103131ad8a7a0abf92e7c5019
James Byrne <james@byrnecodes.com>
2021-04-08 19:00:37 +0100

    Install ember-cli-github-pages addon

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'archer-review',
     environment,
-    rootURL: '/',
+    rootURL: 'archer-review',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
